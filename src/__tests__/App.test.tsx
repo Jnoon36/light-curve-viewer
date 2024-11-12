2710709dca2d3611d089496b4ef7a87997491eed
Jacob <Jacob.noon36@gmail.com>
2024-11-11 22:46:08 -0500

    Added dependencies, add unit testing, added workflows

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { test, expect, describe } from 'vitest';
 import App from '../renderer/App';
 
-describe('App', () => {
-  it('should render', () => {
+test('App', () => {
+  describe('should render', () => {
     expect(render(<App />)).toBeTruthy();
   });
 });
